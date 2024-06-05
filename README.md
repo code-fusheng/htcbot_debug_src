3acebd462196adbba18f2aad5e7c7aad023213e3
code-fusheng <2561035977@qq.com>
2024-06-05 10:48:44 +0800

    fix

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @Author: code-fusheng 2561035977@qq.com
  * @Date: 2024-05-27 10:01:42
  * @LastEditors: code-fusheng 2561035977@qq.com
- * @LastEditTime: 2024-05-29 15:15:16
+ * @LastEditTime: 2024-06-05 10:48:07
  * @FilePath: /src/README.md
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -27,6 +27,3 @@ pip install apscheduler
 # apscheduler 高版本存在 python 2.7 用不了 datetime.datetime
 pip install apscheduler==3.7.0 -i https://pypi.tuna.tsinghua.edu.cn/simple
 ```
-
-
-
--- plane_fit_ground_filter/src/plane_ground_filter_core.cpp
+++ plane_fit_ground_filter/src/plane_ground_filter_core.cpp
@@ -1,8 +1,5 @@
 #include "plane_ground_filter_core.h"
 
-
-
-
 PlaneGroundFilter::PlaneGroundFilter(ros::NodeHandle &nh)
 {
     std::string input_topic;
@@ -48,21 +45,22 @@ PlaneGroundFilter::PlaneGroundFilter(ros::NodeHandle &nh)
 }
 
 PlaneGroundFilter::~PlaneGroundFilter() {}
-bool point_cmp(VPoint a, VPoint b){//比较z轴高度用
+bool point_cmp(VPoint a, VPoint b)
+{ // 比较z轴高度用
     return a.z < b.z;
 }
 void PlaneGroundFilter::Spin()
 {
 }
-//去除过高的点云
+// 去除过高的点云
 void PlaneGroundFilter::clip_above(const pcl::PointCloud<VPoint>::Ptr in,
                                    const pcl::PointCloud<VPoint>::Ptr out)
 {
-    pcl::ExtractIndices<VPoint> cliper;//创建用于提取点的对象
+    pcl::ExtractIndices<VPoint> cliper; // 创建用于提取点的对象
 
-    cliper.setInputCloud(in);//设置将要处理的点云
-    pcl::PointIndices indices;//用于存储要移除点索引的结构体
-#pragma omp for//加速并行处理
+    cliper.setInputCloud(in);                      // 设置将要处理的点云
+    pcl::PointIndices indices;                     // 用于存储要移除点索引的结构体
+#pragma omp for                                    // 加速并行处理
     for (size_t i = 0; i < in->points.size(); i++) // 检查每个点的z坐标是否高于clip_height_，如果是，则添加到索引列表中
     {
         if (in->points[i].z > clip_height_)
@@ -70,13 +68,12 @@ void PlaneGroundFilter::clip_above(const pcl::PointCloud<VPoint>::Ptr in,
             indices.indices.push_back(i);
         }
     }
-    cliper.setIndices(boost::make_shared<pcl::PointIndices>(indices));// 将收集到的索引设置为ExtractIndices对象的索引
-    cliper.setNegative(true); //true是移除
-    cliper.filter(*out);// 执行过滤，根据设置的索引移除点云中的点，结果存储在输出点云out中
+    cliper.setIndices(boost::make_shared<pcl::PointIndices>(indices)); // 将收集到的索引设置为ExtractIndices对象的索引
+    cliper.setNegative(true);                                          // true是移除
+    cliper.filter(*out);                                               // 执行过滤，根据设置的索引移除点云中的点，结果存储在输出点云out中
 }
 
-
-//移除太近的点
+// 移除太近的点
 void PlaneGroundFilter::remove_close_far_pt(const pcl::PointCloud<VPoint>::Ptr in,
                                             const pcl::PointCloud<VPoint>::Ptr out)
 {
@@ -86,7 +83,7 @@ void PlaneGroundFilter::remove_close_far_pt(const pcl::PointCloud<VPoint>::Ptr i
 #pragma omp for
     for (size_t i = 0; i < in->points.size(); i++)
     {
-        double distance = sqrt(in->points[i].x * in->points[i].x + in->points[i].y * in->points[i].y);//点到原点的距离
+        double distance = sqrt(in->points[i].x * in->points[i].x + in->points[i].y * in->points[i].y); // 点到原点的距离
 
         if ((distance < min_distance_) || (distance > max_distance_))
         {
@@ -94,36 +91,32 @@ void PlaneGroundFilter::remove_close_far_pt(const pcl::PointCloud<VPoint>::Ptr i
         }
     }
     cliper.setIndices(boost::make_shared<pcl::PointIndices>(indices));
-    cliper.setNegative(true); //true 是移除
+    cliper.setNegative(true); // true 是移除
     cliper.filter(*out);
 }
 
-
-
 void PlaneGroundFilter::estimate_plane_(void)
 {
-    
-    Eigen::Matrix3f cov;   //协方差
-    Eigen::Vector4f pc_mean; //点云三维平均值
-    pcl::computeMeanAndCovarianceMatrix(*g_ground_pc, cov, pc_mean);// 计算协方差
-    
-    JacobiSVD<MatrixXf> svd(cov, Eigen::DecompositionOptions::ComputeFullU);// SVD分解
-   
-    normal_ = (svd.matrixU().col(2));//选择SVD分解后的左奇异矩阵的第三列向量（方差最小列），作为法线的方向
-   
-    Eigen::Vector3f seeds_mean = pc_mean.head<3>();//点云三维平均值的矩阵
 
+    Eigen::Matrix3f cov;                                             // 协方差
+    Eigen::Vector4f pc_mean;                                         // 点云三维平均值
+    pcl::computeMeanAndCovarianceMatrix(*g_ground_pc, cov, pc_mean); // 计算协方差
 
-    d_ = -(normal_.transpose() * seeds_mean)(0, 0);//根据法线和平均点计算平面方程的常数项
-   
+    JacobiSVD<MatrixXf> svd(cov, Eigen::DecompositionOptions::ComputeFullU); // SVD分解
+
+    normal_ = (svd.matrixU().col(2)); // 选择SVD分解后的左奇异矩阵的第三列向量（方差最小列），作为法线的方向
+
+    Eigen::Vector3f seeds_mean = pc_mean.head<3>(); // 点云三维平均值的矩阵
+
+    d_ = -(normal_.transpose() * seeds_mean)(0, 0); // 根据法线和平均点计算平面方程的常数项
+
     th_dist_d_ = th_dist_ - d_;
     float distance_to_plane = std::abs(d_);
 }
 
-
-void PlaneGroundFilter::extract_initial_seeds_(const pcl::PointCloud<VPoint> &p_sorted)//选择最初的种子点
+void PlaneGroundFilter::extract_initial_seeds_(const pcl::PointCloud<VPoint> &p_sorted) // 选择最初的种子点
 {
-    
+
     double sum = 0;
     int cnt = 0;
     for (int i = 0; i < p_sorted.points.size() && cnt < num_lpr_; i++)
@@ -132,13 +125,13 @@ void PlaneGroundFilter::extract_initial_seeds_(const pcl::PointCloud<VPoint> &p_
         cnt++;
     }
     double lpr_height = cnt != 0 ? sum / cnt : 0; // 计算平均高度，同时避免分母为0
-    g_seeds_pc->clear();//更新
-    
+    g_seeds_pc->clear();                          // 更新
+
     for (int i = 0; i < p_sorted.points.size(); i++)
     {
         if (p_sorted.points[i].z < lpr_height + th_seeds_)
         {
-            g_seeds_pc->points.push_back(p_sorted.points[i]);//将低于平均高度的种子点都当作下一步平面点
+            g_seeds_pc->points.push_back(p_sorted.points[i]); // 将低于平均高度的种子点都当作下一步平面点
         }
     }
 }
@@ -158,9 +151,8 @@ void PlaneGroundFilter::point_cb(const sensor_msgs::PointCloud2ConstPtr &in_clou
     pcl::PointCloud<VPoint> laserCloudIn_org;
     pcl::fromROSMsg(*in_cloud_ptr, laserCloudIn_org);
 
-    
-    SLRPointXYZIRL point;//定义点
-//接收点云,将点云的标签都设置为0，经过后续的计算，如果点云是地面点，将标签设置为1
+    SLRPointXYZIRL point; // 定义点
+    // 接收点云,将点云的标签都设置为0，经过后续的计算，如果点云是地面点，将标签设置为1
     for (size_t i = 0; i < laserCloudIn.points.size(); i++)
     {
         point.x = laserCloudIn.points[i].x;
@@ -173,9 +165,9 @@ void PlaneGroundFilter::point_cb(const sensor_msgs::PointCloud2ConstPtr &in_clou
     }
 
     std::vector<int> indices;
-    pcl::removeNaNFromPointCloud(laserCloudIn, laserCloudIn,indices);//移除无效点
+    pcl::removeNaNFromPointCloud(laserCloudIn, laserCloudIn, indices); // 移除无效点
     // 2.Sort on Z-axis value.
-    sort(laserCloudIn.points.begin(), laserCloudIn.end(), point_cmp);//按Z轴进行排序
+    sort(laserCloudIn.points.begin(), laserCloudIn.end(), point_cmp); // 按Z轴进行排序
 
     pcl::PointCloud<VPoint>::iterator it = laserCloudIn.points.begin();
     for (int i = 0; i < laserCloudIn.points.size(); i++)
@@ -189,7 +181,7 @@ void PlaneGroundFilter::point_cb(const sensor_msgs::PointCloud2ConstPtr &in_clou
             break;
         }
     }
-    laserCloudIn.points.erase(laserCloudIn.points.begin(), it);//低于1.5倍传感器高度的点云被滤除了
+    laserCloudIn.points.erase(laserCloudIn.points.begin(), it); // 低于1.5倍传感器高度的点云被滤除了
 
     // 4. 提取地面种子点
     extract_initial_seeds_(laserCloudIn);
@@ -197,20 +189,20 @@ void PlaneGroundFilter::point_cb(const sensor_msgs::PointCloud2ConstPtr &in_clou
     // 5. 循环
     for (int i = 0; i < num_iter_; i++)
     {
-        estimate_plane_();//拟合平面
+        estimate_plane_(); // 拟合平面
         g_ground_pc->clear();
         g_not_ground_pc->clear();
 
-        MatrixXf points(laserCloudIn_org.points.size(), 3);//点云xyz矩阵
+        MatrixXf points(laserCloudIn_org.points.size(), 3); // 点云xyz矩阵
         int j = 0;
         for (auto p : laserCloudIn_org.points)
         {
             points.row(j++) << p.x, p.y, p.z;
         }
-    
-        VectorXf result = points * normal_;//点和法向量的点积，即点到平面的距离
 
-        for (int r = 0; r < result.rows(); r++)//
+        VectorXf result = points * normal_; // 点和法向量的点积，即点到平面的距离
+
+        for (int r = 0; r < result.rows(); r++) //
         {
             if (result[r] < th_dist_d_)
             {
