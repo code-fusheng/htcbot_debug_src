6f2a36a80751831640e2829f665cb363526f7fe9
code-fusheng <2561035977@qq.com>
2024-06-16 10:59:11 +0800

    x

--- htc_gateway/scripts/htc_cloud_websocket.py
+++ htc_gateway/scripts/htc_cloud_websocket.py
@@ -4,7 +4,7 @@
 Author: code-fusheng 2561035977@qq.com
 Date: 2024-05-30 22:25:27
 LastEditors: code-fusheng 2561035977@qq.com
-LastEditTime: 2024-06-13 14:34:26
+LastEditTime: 2024-06-13 18:31:55
 FilePath: /src/htc_gateway/scripts/htc_cloud_websocket.py
 # pip3 install opencv-python -i https://pypi.tuna.tsinghua.edu.cn/simple
 # pip3 install scikit-build
@@ -37,20 +37,21 @@ class HtcCloudWebSocketNode:
         self.srv_switch_status = rospy.Service("~set_switch_status", SwitchStatusSrv, self.switch_status_callback)
         self.device_ports = ["/dev/camera_front"]
         self.image_subs = []
+        self.websocket = None
 
-        
-        self.websocket_uri = "ws://124.223.72.28:10240/robot/push/websocket"
-        self.websocket_thread = threading.Thread(target=self.start_websocket_loop)
-        self.websocket_thread.start()
+        self.websocket_ip = "124.223.72.28"
+        self.websocket_uri = "ws://124.223.72.28:10240/robot/push/websocket?sid=car&vin=HTCBOT_A0003"
+        # self.websocket_thread = threading.Thread(target=self.start_websocket_loop)
+        # self.websocket_thread.start()
+
+        self.check_connection_thread = threading.Thread(target=self.start_websocket_loop)
+        self.check_connection_thread.start()
 
     def run(self):
         self.init()
-        # rate = rospy.Rate(1)  # 1 Hz
-        # while not rospy.is_shutdown():  # 添加条件
-            # Perform your main operations here
-            # rospy.loginfo("[htc_cloud_websocket_node] ===> Running...")
-            # rate.sleep()
-        rospy.spin()
+        rate = rospy.Rate(1)  # 1 Hz
+        while not rospy.is_shutdown():  # 添加条件
+            rate.sleep()
 
     def init(self):
         # init connect websocket
@@ -86,21 +87,50 @@ class HtcCloudWebSocketNode:
         asyncio.run_coroutine_threadsafe(self.send_image(image_bytes), self.websocket_loop)
 
     def start_websocket_loop(self):
+        print("xxxxxxx")
         self.websocket_loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.websocket_loop)
-        self.websocket_loop.run_until_complete(self.connect_to_websocket())
+        self.websocket_loop.run_until_complete(self.check_websocket_connection())
+        self.websocket_loop.run_forever()
+
+    async def check_websocket_connection(self):
+        print("yyyyyyy")
+        while not rospy.is_shutdown():
+            print("zzzzz")
+            print(self.websocket)
+            if self.websocket is None:
+                rospy.logwarn("WebSocket connection lost, attempting to reconnect...")
+                await self.connect_to_websocket()
+            else:
+                rospy.loginfo("Ws connected")
+            await asyncio.sleep(1)
 
     async def connect_to_websocket(self):
-        async with websockets.connect(self.websocket_uri) as websocket:
-            self.websocket = websocket
-            while not rospy.is_shutdown():
-                await asyncio.sleep(1)
+        try:
+            self.websocket = await websockets.connect(self.websocket_uri)
+            rospy.loginfo("WebSocket connection established")
+            asyncio.ensure_future(self.receive_messages())  # 启动接收消息的任务
+        except Exception as e:
+            rospy.logerr(f"Failed to connect to WebSocket: {e}")
+
+    async def receive_messages(self):
+        try:
+            async for message in self.websocket:
+                rospy.loginfo(f"Received message: {message}")
+                # Handle incoming message
+        except websockets.ConnectionClosed:
+            rospy.logwarn("WebSocket connection closed during message reception")
+        except Exception as e:
+            rospy.logerr(f"Error receiving WebSocket messages: {e}")
 
     async def send_image(self, image_bytes):
-        if self.websocket.open:
-            await self.websocket.send(image_bytes)
-        else:
-            rospy.logwarn("WebSocket connection is not open")
+        if self.websocket and self.websocket.open:
+            try:
+                await self.websocket.send(image_bytes)
+            except websockets.ConnectionClosed:
+                rospy.logwarn("WebSocket connection is not open")
+            except Exception as e:
+                rospy.logerr(f"Error sending image: {e}")
 
 if __name__ == '__main__':
     try:
